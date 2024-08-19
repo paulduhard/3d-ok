30c5930337a17adce31162b160681f15a44dfffc
Paul <paul@duhard.com>
2024-08-19 20:59:55 +0200

    fix broken build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,7 +31,7 @@
 	<slot />
 </main>
 <div class="absolute inset-0 max-h-screen background-gradient -z-50"></div>
-<div class="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>	
+<div class="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>	
 
 <Footer settings={data.settings} />
 
